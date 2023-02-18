dc2a8f35ce981ed0521c41618770ff4cd94dfa8e
李建彤 <1791224624@qq.com>
2023-02-18 20:52:48 +0800

    update 序列化处理

--- rpc-framework-core/src/main/java/com/bill/remoting/transport/netty/codec/MessageEncoder.java
+++ rpc-framework-core/src/main/java/com/bill/remoting/transport/netty/codec/MessageEncoder.java
@@ -2,6 +2,8 @@ package com.bill.remoting.transport.netty.codec;
 
 import com.bill.remoting.constants.RpcConstants;
 import com.bill.remoting.dto.Message;
+import com.bill.serialize.Serializer;
+import com.bill.serialize.impl.KryoSerializer;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
@@ -9,7 +11,9 @@ import io.netty.handler.codec.MessageToByteEncoder;
 public class MessageEncoder extends MessageToByteEncoder<Message> {
 
     /**
+     * 传输协议
      * 4B magic_number + 1B version + 4B full_length + 1B message_type + 1B codec + 1B compress + 4B request_id + NB body
+     * netty 传输编码器
      */
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, Message message, ByteBuf byteBuf) throws Exception {
@@ -21,7 +25,12 @@ public class MessageEncoder extends MessageToByteEncoder<Message> {
                 .writeByte(message.getCompress())
                 .writeInt(message.getRequestId());
         Object data = message.getData();
-
-
+        Serializer serializer = new KryoSerializer();
+        byte[] bytes = serializer.serialize(data);
+        //todo 数据压缩部分
+        byteBuf.writeBytes(bytes);
+        int fullLength = bytes.length;
+        int curIdx = byteBuf.writerIndex();
+        byteBuf.writerIndex(idx).writeInt(fullLength).writeInt(curIdx);
     }
 }
